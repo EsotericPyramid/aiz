6f61a0e2ed6e2bbc0e8b99dcbe218675a87920be
EsotericPyramid <160424984+EsotericPyramid@users.noreply.github.com>
2023-07-06 11:39:42 -0400

    changed main.rs to be a test on the MNIST dataset

--- src/main.rs
+++ src/main.rs
@@ -1,104 +1,116 @@
 use aiz::aiz; //change the structure so I dont need to do this dumb stuff
 use std::fs;
-use rand::seq::SliceRandom;
 
-fn letter_to_one_hot(letter: char) -> [f64; 26] {
-    match letter {
-        ' ' => [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
-        'a' => [1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
-        'b' => [0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
-        'c' => [0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
-        'd' => [0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
-        'e' => [0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
-        'f' => [0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
-        'g' => [0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
-        'h' => [0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
-        'i' => [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
-        'j' => [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
-        'k' => [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
-        'l' => [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
-        'm' => [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
-        'n' => [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
-        'o' => [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
-        'p' => [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
-        'q' => [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
-        'r' => [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
-        's' => [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
-        't' => [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0],
-        'u' => [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0],
-        'v' => [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0],
-        'w' => [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0],
-        'x' => [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0],
-        'y' => [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0],
-        'z' => [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0],
-         _  => [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
+fn label_to_vec(label: u8) -> Vec<f64> {
+    match label {
+        0 => vec![1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
+        1 => vec![0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
+        2 => vec![0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
+        3 => vec![0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0],
+        4 => vec![0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0],
+        5 => vec![0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0],
+        6 => vec![0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0],
+        7 => vec![0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0],
+        8 => vec![0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0],
+        9 => vec![0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0],
+        _ => panic!("invalid label"),
     }
 }
+
+fn vec_to_label(vec: Vec<f64>) -> u8 {
+    let mut current_num = 0;
+    let mut highest_num = 11;
+    let mut highest_num_value = 0.0;
+    for current_num_value in vec {
+        if current_num_value > highest_num_value {
+            highest_num_value = current_num_value.clone();
+            highest_num = current_num;
+        }
+        current_num += 1;
+    }
+    highest_num
+}
+
+fn read_n_parse_dataset() -> (Vec<(Vec<f64>,Vec<f64>)>,Vec<(Vec<f64>,Vec<f64>)>){
+    //training labels
+    let raw_training_label_file = fs::read("train-labels-idx1-ubyte").expect("failed to read training labels");
+    let mut training_labels = Vec::new();
+    let mut current_byte_num = 0;
+    for label in raw_training_label_file.into_iter() {
+        if current_byte_num >= 8 {
+            training_labels.push(label_to_vec(label));
+        }
+        current_byte_num += 1;
+    }
+    //training images
+    let raw_training_image_file = fs::read("train-images-idx3-ubyte").expect("unable to read training images");
+    let mut training_images = Vec::new();
+    let mut current_image = Vec::new();
+    let mut current_byte_num = 0;
+    let mut current_image_position = 0;
+    for pixel in raw_training_image_file.into_iter() {
+        if current_byte_num >= 16 {
+            current_image.push(pixel as f64/ 256.0f64);
+            current_image_position += 1;
+            if current_image_position == 784 {
+                current_image_position = 0;
+                training_images.push(current_image);
+                current_image = Vec::new();
+            }
+        }
+        current_byte_num += 1;
+    }
+    let training_data = training_images.into_iter().zip(training_labels.into_iter()).collect::<Vec<(Vec<f64>,Vec<f64>)>>();
+    //test labels
+    let raw_test_label_file = fs::read("t10k-labels-idx1-ubyte").expect("failed to read test labels");
+    let mut test_labels = Vec::new();
+    let mut current_byte_num = 0;
+    for label in raw_test_label_file.into_iter() {
+        if current_byte_num >= 8 {
+            test_labels.push(label_to_vec(label));
+        }
+        current_byte_num += 1;
+    }
+    //test images
+    let raw_test_image_file = fs::read("t10k-images-idx3-ubyte").expect("unable to read test images");
+    let mut test_images = Vec::new();
+    let mut current_image = Vec::new();
+    let mut current_byte_num = 0;
+    let mut current_image_position = 0;
+    for pixel in raw_test_image_file.into_iter() {
+        if current_byte_num >= 16 {
+            current_image.push(pixel as f64/ 256.0f64);
+            current_image_position += 1;
+            if current_image_position == 784 {
+                current_image_position = 0;
+                test_images.push(current_image);
+                current_image = Vec::new();
+            }
+        }
+        current_byte_num += 1;
+    }
+    let test_data = test_images.into_iter().zip(test_labels.into_iter()).collect::<Vec<(Vec<f64>,Vec<f64>)>>();
+    (training_data,test_data)
+
+}
+
 fn main() {
-    let mut processed_words = Vec::new();
-    let real_words = fs::read_to_string("wordlist.txt").expect("Couldn't read the file");
-    let mut frequency_array = [0; 29];
-    for real_word in real_words.lines() {
-        frequency_array[real_word.len()] += 1;
-        let mut word_vec = Vec::new();
-        let mut adj_real_word = String::from(real_word);
-        while adj_real_word.len() < 28 {
-            adj_real_word.push_str(" ");
-        }
-        for letter in adj_real_word.chars() {
-            for index in letter_to_one_hot(letter){
-                word_vec.push(index);
-            }
-        }
-        processed_words.push((word_vec,vec![1.0,0.0]))
-    }
-
-    println!("real words processed!");
-
-    let letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-    let mut rng = rand::thread_rng();
-    let mut fake_words = Vec::new();
-    for length in 1..29 {
-        for _ in 0..frequency_array[length] {
-            let mut new_word = String::new();
-            for _ in 0..length {
-                new_word.push_str(letters.choose(&mut rng).unwrap());
-            }
-            for _ in 0..(28-length) {
-                new_word.push_str(" ");
-            }
-            fake_words.push(new_word);
+    let (training_data,test_data) = read_n_parse_dataset();
+    println!("all data processed");
+    let mut nn = aiz::NeuralNetwork::new(vec![784,20,20,10]);
+    nn.back_propagation(&training_data, &test_data, (-30.0_f64).exp2());
+    let mut num_correct = 0;
+    for example in training_data {
+        if vec_to_label(nn.run(&example.0)) == vec_to_label(example.1) {
+            num_correct += 1;
         }
     }
-
-    println!("fake words created!");
-
-    for fake_word in fake_words {
-        let mut word_vec = Vec::new();
-        for letter in fake_word.chars() {
-            for index in letter_to_one_hot(letter){
-                word_vec.push(index);
-            }
-        }
-        processed_words.push((word_vec,vec![0.0,1.0]))
-    }
-
-    println!("fake words processed!");
-
-    processed_words.shuffle(&mut rng);
-    let mut training_data = Vec::new();
-    let mut test_data = Vec::new();
-    for (index,example) in processed_words.into_iter().enumerate() {
-        if index < 10000 {
-            test_data.push(example)
-        } else {
-            training_data.push(example)
+    println!("{}",num_correct);
+    let mut num_correct = 0;
+    for example in test_data {
+        if vec_to_label(nn.run(&example.0)) == vec_to_label(example.1) {
+            num_correct += 1;
         }
     }
-
-    println!("All examples processed and split up!");
-
-    let mut is_word_neural_network = aiz::NeuralNetwork::new(vec![728,100,100,2]);
-    println!("Beginning Training!!!");
-    is_word_neural_network.stochastic_back_propagation( &training_data, &test_data, (-100.0_f64).exp2(), 10000);
+    println!("{}",num_correct);
 }
