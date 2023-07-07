aca202cd09d98df7d7dcde06bb1d9dff2b687a39
EsotericPyramid <160424984+EsotericPyramid@users.noreply.github.com>
2023-07-07 12:26:43 -0400

    Added experimental multithreaded backpropagation uses crossbeam 0.8.2 to accomplish this

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,9 @@
 pub mod aiz {
     use rand::Rng;
     use rand::seq::SliceRandom;
+    use crossbeam;
+    use std::sync::mpsc;
+
     //should be optimizable
     pub fn flip_matrix<T: Copy>(matrix: &Vec<Vec<T>>) -> Vec<Vec<T>> {
         //probably would be good if I could do this with iterators
@@ -417,6 +420,119 @@ pub mod aiz {
             (final_biases_gradient,final_weights_gradient)
         }
         
+        fn core_multithreaded_back_propagation(&self,training_data: &Vec<(Vec<f64>,Vec<f64>)>) -> (Vec<Vec<f64>>,Vec<Vec<Vec<f64>>>) {
+            let length = self.node_layout.len();
+            let mut final_biases_gradient = Vec::with_capacity(length-1);
+            for layer_size in self.node_layout[1..length].iter() {
+                let mut layer_biases = Vec::with_capacity(*layer_size);
+                for _ in 0..*layer_size {
+                    layer_biases.push(0.0); 
+                }
+                final_biases_gradient.push(layer_biases);
+            }
+            let mut final_weights_gradient = Vec::with_capacity(length-1);
+            for (previous_layer_size, layer_size) in self.node_layout[0..length-1].iter().zip(self.node_layout[1..length].iter()) {
+                let mut layer_weights = Vec::with_capacity(*previous_layer_size);
+                for _ in 0..*previous_layer_size {
+                    let mut node_weights = Vec::with_capacity(*layer_size);
+                    for _ in 0..*layer_size {
+                        node_weights.push(0.0);
+                    }
+                    layer_weights.push(node_weights);
+                }
+                final_weights_gradient.push(layer_weights);
+            }
+
+            //multithreading specific stuff
+            let (original_transmitter,reciever) = mpsc::channel();
+            crossbeam::scope(|scope| {
+                for (training_ex_in,training_ex_out) in training_data {
+                    let transmitter = original_transmitter.clone();
+                    scope.spawn(move |_| {
+                        let mut biases_gradient = Vec::new();
+                        let mut weights_gradient = Vec::new();
+                        let layer_vals = self.special_run(training_ex_in);
+                        let mut layer_node_multipliers = Vec::new(); //there may be a specific func for this
+                        for _ in 0..layer_vals[layer_vals.len()-1].len() {
+                            layer_node_multipliers.push(1.0);
+                        }
+                        let temp_layer_vals: Vec<&Vec<f64>> = layer_vals[0..layer_vals.len()].iter().rev().collect(); //JANK, originally to avoid lazyness in python
+                        let mut temp_layer_node_multpliers: Vec<f64>;
+                        for (current_layer_vals,(layer_weights,(layer_biases,(layer_num,previous_layer_vals)))) in 
+                        layer_vals.iter().rev().zip(self.weights.iter().rev().zip(self.biases.iter().rev().zip((0..layer_vals.len()).into_iter().zip(temp_layer_vals.iter())))) {
+                            let mut layer_biases_gradient = Vec::new();
+                            let mut layer_weights_gradient = Vec::new();
+                            temp_layer_node_multpliers = Vec::new();
+                            if layer_num == 0 {
+                                for (out_node_val,(out_node_back_weights,(out_node_bias,out_node_expected_val))) in
+                                current_layer_vals.iter().zip(flip_matrix(layer_weights).iter().zip(layer_biases.iter().zip(training_ex_out.iter()))) {
+                                    let out_node_multiplier = self.derivative_activation_function(previous_layer_vals.iter()
+                                                                                                        .zip(out_node_back_weights.iter())
+                                                                                                        .map(|(previous_activation,weight)| previous_activation*weight)
+                                                                                                        .sum::<f64>() 
+                                                                                                        + out_node_bias) * 2.0 * (out_node_val-out_node_expected_val);
+                                    layer_biases_gradient.push(out_node_multiplier);
+                                    layer_weights_gradient.push(previous_layer_vals.iter().map(|in_node_activation| in_node_activation*out_node_multiplier).collect::<Vec<f64>>());
+                                    temp_layer_node_multpliers.push(out_node_multiplier);
+                                }
+                            } else {
+                                for (out_node_back_weights,(out_node_bias,out_node_multiplier)) in 
+                                flip_matrix(layer_weights).iter().zip(layer_biases.iter().zip(layer_node_multipliers.iter())) {
+                                    let out_node_multiplier = self.derivative_activation_function(previous_layer_vals.iter()
+                                                                                                        .zip(out_node_back_weights.iter())
+                                                                                                        .map(|(previous_activation,weight)| previous_activation*weight)
+                                                                                                        .sum::<f64>() 
+                                                                                                        + out_node_bias) * out_node_multiplier;
+                                    layer_biases_gradient.push(out_node_multiplier);
+                                    layer_weights_gradient.push(previous_layer_vals.iter().map(|in_node_activation| in_node_activation*out_node_multiplier).collect::<Vec<f64>>());
+                                    temp_layer_node_multpliers.push(out_node_multiplier);
+                                }
+                            }
+                            biases_gradient.push(layer_biases_gradient);
+                            weights_gradient.push(layer_weights_gradient);
+                            let mut new_layer_node_multipliers = Vec::new();
+                            for in_node in layer_weights {
+                                new_layer_node_multipliers.push(in_node.iter().zip(temp_layer_node_multpliers.iter()).map(|(weight,multiplier)| weight*multiplier).sum::<f64>());
+                            }
+                            layer_node_multipliers = new_layer_node_multipliers;
+
+                        }
+                        transmitter.send((biases_gradient,weights_gradient)).unwrap();
+                    });
+                }
+
+                for (biases_gradient,weights_gradient) in reciever {
+                    for (column_bias_gradient,final_column_bias_gradient) in biases_gradient.into_iter().rev().zip(final_biases_gradient.iter_mut()) {
+                        for (bias_gradient,final_bias_gradient) in column_bias_gradient.into_iter().zip(final_column_bias_gradient.iter_mut()) {
+                            *final_bias_gradient += bias_gradient;
+                        }
+                    }
+                    for (layer_weights_gradient,final_layer_weights_gradient) in weights_gradient.into_iter().rev().zip(final_weights_gradient.iter_mut()) {
+                        for (in_node_weights_gradient, final_in_node_weights_gradient) in layer_weights_gradient.into_iter().zip(final_layer_weights_gradient.iter_mut()) {
+                            for (weight_gradient,final_weight_gradient) in in_node_weights_gradient.into_iter().zip(final_in_node_weights_gradient.iter_mut()) {
+                                *final_weight_gradient += weight_gradient;
+                            }
+                        }
+                    }
+                }
+            }).unwrap();
+            let num_examples = training_data.len() as f64;
+            for column in final_biases_gradient.iter_mut() {
+                for bias in column.iter_mut() {
+                    *bias /= num_examples;
+                }
+            }
+            for layer in final_weights_gradient.iter_mut() {
+                for in_node in layer.iter_mut() {
+                    for weight in in_node.iter_mut() {
+                        *weight /= num_examples;
+                    }
+                }
+            }
+
+            (final_biases_gradient,final_weights_gradient)
+        }
+
         pub fn back_propagation(&mut self, training_data: &Vec<(Vec<f64>,Vec<f64>)>, test_data: &Vec<(Vec<f64>,Vec<f64>)>, min_granularity: f64, is_silent: bool) {
             let mut current_granularity = 1.0;
             //double testing done here, would be nice to avoid it
@@ -555,6 +671,94 @@ pub mod aiz {
                     if !is_silent {
                         println!("Granularity: {}",current_granularity);
                         println!("Min granularity: {}",min_granularity);
+                    }
+                    multiplier /= 2.0 ;
+                    if current_granularity < min_granularity {
+                        for (column_biases, column_gradient) in self.biases.iter_mut().zip(biases_gradient.iter()) {
+                            for (bias, movement) in column_biases.iter_mut().zip(column_gradient.iter()) {
+                                *bias += movement*multiplier*2.0;
+                            }
+                        }
+                        for (layer_weights, layer_gradient) in self.weights.iter_mut().zip(weights_gradient.iter()) {
+                            for (in_node_weights,in_node_gradient) in layer_weights.iter_mut().zip(layer_gradient.iter()) {
+                                for (weight, movement) in in_node_weights.iter_mut().zip(in_node_gradient.iter()) {
+                                    *weight += movement*multiplier*2.0;
+                                }
+                            }
+                        }
+                        break 'main_loop;
+                    }
+                    for (column_biases, column_gradient) in self.biases.iter_mut().zip(biases_gradient.iter()) {
+                        for (bias, movement) in column_biases.iter_mut().zip(column_gradient.iter()) {
+                            *bias += movement*multiplier;
+                        }
+                    }
+                    for (layer_weights, layer_gradient) in self.weights.iter_mut().zip(weights_gradient.iter()) {
+                        for (in_node_weights,in_node_gradient) in layer_weights.iter_mut().zip(layer_gradient.iter()) {
+                            for (weight, movement) in in_node_weights.iter_mut().zip(in_node_gradient.iter()) {
+                                *weight += movement*multiplier;
+                            }
+                        }
+                    }
+                    new_test = self.test(test_data);
+                    if !is_silent {
+                        println!("Test: {}",new_test);
+                    }
+                }
+                previous_test = new_test;
+            }
+        }
+    
+        pub fn multithreaded_back_propagation(&mut self, training_data: &Vec<(Vec<f64>,Vec<f64>)>, test_data: &Vec<(Vec<f64>,Vec<f64>)>, min_granularity: f64, is_silent: bool) {
+            let mut current_granularity = 1.0;
+            //double testing done here, would be nice to avoid it
+            let mut previous_test = self.test(test_data);
+            if !is_silent {
+                println!("Test: {}",previous_test);
+            }
+            //not using previous_biases and previous_weights here to avoid cloning, needs testing to see if better
+            //theoretically has worse precision
+            'main_loop: loop {
+                let (biases_gradient,weights_gradient) = self.core_multithreaded_back_propagation(training_data);
+                let mut greatest_movement = 0.0;
+                for column in biases_gradient.iter() {
+                    for bias in column {
+                        if &greatest_movement < bias {
+                            greatest_movement = *bias;
+                        }
+                    }
+                }
+                for layer in weights_gradient.iter() {
+                    for in_node in layer {
+                        for weight in in_node {
+                            if &greatest_movement < weight {
+                                greatest_movement = *weight;
+                            }
+                        }
+                    }
+                }
+                let mut multiplier = current_granularity/greatest_movement;
+                for (column_biases, column_gradient) in self.biases.iter_mut().zip(biases_gradient.iter()) {
+                    for (bias, movement) in column_biases.iter_mut().zip(column_gradient.iter()) {
+                        *bias -= movement*multiplier;
+                    }
+                }
+                for (layer_weights, layer_gradient) in self.weights.iter_mut().zip(weights_gradient.iter()) {
+                    for (in_node_weights,in_node_gradient) in layer_weights.iter_mut().zip(layer_gradient.iter()) {
+                        for (weight, movement) in in_node_weights.iter_mut().zip(in_node_gradient.iter()) {
+                            *weight -= movement*multiplier;
+                        }
+                    }
+                }
+                let mut new_test = self.test(test_data);
+                if !is_silent {
+                    println!("Test: {}",new_test);
+                }
+                while new_test > previous_test {
+                    current_granularity /= 2.0;
+                    if !is_silent {
+                        println!("Granularity: {}",current_granularity);
+                        println!("Min Granularity: {}",min_granularity);
                     }
                     multiplier /= 2.0 ;
                     if current_granularity < min_granularity {
